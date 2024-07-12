54afa53f9f62954e3cc544e267f61441f5416055
Ariel Pont <arielpont@users.noreply.github.com>
2024-07-12 17:55:35 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -64,7 +64,7 @@ input.onGesture(Gesture.Shake, function () {
         rollDice()
     }
 })
-function start () {
+function startAnimation () {
     for (let index = 0; index < 1; index++) {
         led.toggle(2, 2)
         basic.pause(250)
@@ -73,8 +73,9 @@ function start () {
 let random_number = 0
 let isShakeing = false
 isShakeing = false
+random_number = 0
 basic.forever(function () {
     while (isShakeing == false) {
-        start()
+        startAnimation()
     }
 })
